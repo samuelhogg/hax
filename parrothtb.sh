5ecd312df10e622dbe563b8dc6f082e5d824cfa2
samuelhogg <45640681+samuelhogg@users.noreply.github.com>
2024-07-08 18:03:26 +0000

    Update parrothtb.sh

--- parrothtb.sh
+++ parrothtb.sh
@@ -1,31 +1,25 @@
 #!/bin/sh
 
-#Change to UK Keyboard
+##### chmod +x ~/Downloads/parrothtb.sh
 
-setxkbmap gb
+##### Settings #####
 
-#Change Toolbar Buttons to right hand side
+setxkbmap gb    #Change to UK Keyboard
 
-gsettings set org.mate.Marco.general button-layout 'menu:minimize,maximize,close'
+gsettings set org.mate.Marco.general button-layout 'menu:minimize,maximize,close'   #Change Toolbar Buttons to right hand side
 
-#Change Icon Theme to hackthebox
+gsettings set org.mate.interface icon-theme hackthebox #Change Icon Theme to hackthebox 
 
-gsettings set org.mate.interface icon-theme hackthebox
+gsettings set org.mate.background picture-filename /usr/share/backgrounds/hackthebox-alt.jpg    #Change Background picture
 
-#Change Background picture
-
-gsettings set org.mate.background picture-filename /usr/share/backgrounds/hackthebox-alt.jpg
-
+##### Install #####
 
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O ~/Downloads/chrome.deb && sudo apt install -y ~/Downloads/chrome.deb
 
-
-##############################################################################################################
+##### Alt Backgrounds #####
 
 #/usr/share/backgrounds/hackthebox.jpg  
 #/usr/share/backgrounds/hackthebox-alt.jpg
 #/usr/share/backgrounds/htb.jpg
 #/usr/share/backgrounds/htb-alt.jpg
 #/usr/share/backgrounds/macaw-vaporwave.jpg
-
-#########
