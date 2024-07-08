41df1329a959e714f6bb471ea035fa740d187c23
samuelhogg <45640681+samuelhogg@users.noreply.github.com>
2024-07-08 16:33:33 +0000

    Update parrothtb.sh

--- parrothtb.sh
+++ parrothtb.sh
@@ -16,6 +16,12 @@ gsettings set org.mate.interface icon-theme hackthebox
 
 gsettings set org.mate.background picture-filename /usr/share/backgrounds/hackthebox-alt.jpg
 
+
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O ~/Downloads/chrome.deb && sudo apt install -y ~/Downloads/chrome.deb
+
+
+##############################################################################################################
+
 #/usr/share/backgrounds/hackthebox.jpg  
 #/usr/share/backgrounds/hackthebox-alt.jpg
 #/usr/share/backgrounds/htb.jpg
